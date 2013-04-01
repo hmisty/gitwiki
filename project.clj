3558accf3804661762f602308c137f04aa22b448
Evan Liu <hmisty@gmail.com>
2013-04-01 13:35:18 +0800

    basic auth

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring "1.0.1"]
+                 [ring-basic-authentication "1.0.2"]
                  [compojure "1.0.1"]
                  [enlive "1.0.0"]]
   :main gitwiki.server 
--- src/gitwiki/core.clj
+++ src/gitwiki/core.clj
@@ -1,30 +1,54 @@
 (ns gitwiki.core
   (:gen-class)
   (:use [clojure.pprint]
-        [compojure.core :only (GET POST defroutes)])
+        [compojure.core :only (GET POST defroutes)]
+        [ring.middleware.basic-authentication :as auth])
   (:require [ring.util.response :as resp]
             [net.cgrand.enlive-html :as en]
             [compojure.handler]
             [compojure.route]))
 
 ;; some configs
+(def PROJECT "GitWiki")
 (def DEFAULT_PAGE "Home")
 (def THEME "default")
+
+;; helpers
+(defmacro page_url
+  [page]
+  `(str "/wiki/" ~page))
+
+(defmacro history_url
+  ([] (str "/history"))
+  ([page] (str "/history/" page)))
+
+(defn authenticated? [name pass]
+  (and (= name "test") (= pass "test")))
 
 ;; the pages
 (en/deftemplate view
   (en/xml-resource (str THEME "/view.html"))
-  [page]
-  [:title] (en/content page)
-  [:home_url] (en/content "some url"))
+  [& page]
+  [:title] (en/content [PROJECT " > " page])
+  [:a.home_url] (en/set-attr :href (page_url DEFAULT_PAGE))
+  [:a.global_history_url] (en/set-attr :href (history_url)))
+
+(en/deftemplate history
+  (en/xml-resource (str THEME "/history.html"))
+  [& page]
+  [:title] (en/content [PROJECT " > History " page])
+  [:a.home_url] (en/set-attr :href (page_url DEFAULT_PAGE))
+  [:a.global_history_url] (en/set-attr :href (history_url)))
 
 ;; the handlers
 (defroutes handler
   ;; view
-  (GET "/" req (view DEFAULT_PAGE))
+  (GET "/" req (view))
   (GET "/wiki/:page" [page] (view page))
   ;; edit
   ;; history
+  (GET "/history" req (history))
+  (GET "/history/:page" [page] (history page))
   ;; static resources
   (compojure.route/resources "/" {:root THEME})
   ;; default route
@@ -35,4 +59,5 @@
 ;; the web app
 (def app
   (-> handler
-      (compojure.handler/site)))
+      (compojure.handler/site)
+      (wrap-basic-authentication authenticated?)))
