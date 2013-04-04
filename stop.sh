2ab7e9cb9281e6baf8bc099dff9edf15f5e6e2d3
Evan Liu <hmisty@gmail.com>
2013-04-04 17:02:50 +0800

    change to signal 15

--- stop.sh
+++ stop.sh
@@ -5,7 +5,7 @@ JAR=$DIR/gitwiki-0.1.0-SNAPSHOT-standalone.jar
 LOG=$DIR/gitwiki.log
 PID=$DIR/gitwiki.pid
 
-kill -0 `cat "$PID"` >> $LOG 2>&1
+kill -15 `cat "$PID"` >> $LOG 2>&1
 if [ $? -eq 0 ]; then
 	echo Exit signal sent successfully.
 else
