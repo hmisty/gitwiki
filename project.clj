07d9bf9f3cfb12f6c09b91faf71d5a2882ce9c97
Evan Liu <hmisty@gmail.com>
2013-06-12 23:33:31 +0800

    0.4.0 - added new theme

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gitwiki "0.3.0"
+(defproject gitwiki "0.4.0"
   :description "gitwiki: a simple, light and easy to use wiki with git backend"
   :url "http://github.com/hmisty/gitwiki"
   :license {:name "Eclipse Public License"
